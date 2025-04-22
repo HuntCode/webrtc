479977a43fdebeab30dc46260944a84456cbca12
wanglu3@honghe-tech.com <wlhuntcode@gmail.com>
2025-04-22 14:05:38 +0800

    [QtTestDriver] 1.提交文件

---
+++ qt_test_driver/QtTestDriver/SignalingSocket.cpp
@@ -0,0 +1,74 @@
+﻿#include <QDebug>
+
+#include "SignalingSocket.h"
+
+SignalingSocket::SignalingSocket(QObject* parent)
+    : QObject(parent) {
+  server_ = new QTcpServer(this);
+  connect(server_, &QTcpServer::newConnection, this, &SignalingSocket::onNewConnection);
+}
+
+void SignalingSocket::startListening(quint16 port) {
+  if (!server_->listen(QHostAddress::Any, port)) {
+    emit errorOccurred("监听失败: " + server_->errorString());
+    return;
+  }
+  qDebug() << u8"监听中，端口：" << port;
+}
+
+void SignalingSocket::connectToHost(const QString& ip, quint16 port) {
+  socket_ = new QTcpSocket(this);
+  connect(socket_, &QTcpSocket::readyRead, this, &SignalingSocket::onReadyRead);
+  connect(socket_, &QTcpSocket::disconnected, this, &SignalingSocket::onSocketDisconnected);
+  connect(socket_, QOverload<QAbstractSocket::SocketError>::of(&QTcpSocket::errorOccurred), this, &SignalingSocket::onSocketError);
+  connect(socket_, &QTcpSocket::connected, this, &SignalingSocket::connected);
+
+  socket_->connectToHost(ip, port);
+}
+
+void SignalingSocket::onNewConnection() {
+  if (socket_)
+    return;  // 已连接，忽略新的连接
+
+  socket_ = server_->nextPendingConnection();
+  connect(socket_, &QTcpSocket::readyRead, this, &SignalingSocket::onReadyRead);
+  connect(socket_, &QTcpSocket::disconnected, this, &SignalingSocket::onSocketDisconnected);
+  connect(socket_,
+      QOverload<QAbstractSocket::SocketError>::of(&QTcpSocket::errorOccurred),
+      this, &SignalingSocket::onSocketError);
+
+  emit connected();
+  qDebug() << u8"客户端连接成功";
+}
+
+void SignalingSocket::sendMessage(const QString& json) {
+  if (!socket_)
+    return;
+  socket_->write(json.toUtf8() + '\n');  // 以换行符分隔消息
+}
+
+void SignalingSocket::onReadyRead() {
+  buffer_ += socket_->readAll();
+  while (true) {
+    int newlineIndex = buffer_.indexOf('\n');
+    if (newlineIndex == -1)
+      break;
+
+    QByteArray message = buffer_.left(newlineIndex);
+    buffer_ = buffer_.mid(newlineIndex + 1);
+    emit messageReceived(QString::fromUtf8(message));
+  }
+}
+
+void SignalingSocket::onSocketDisconnected() {
+  qDebug() << u8"连接已断开";
+  emit disconnected();
+  socket_->deleteLater();
+  socket_ = nullptr;
+}
+
+void SignalingSocket::onSocketError(
+    QAbstractSocket::SocketError socketError) {
+  Q_UNUSED(socketError);
+  emit errorOccurred(socket_->errorString());
+}
---
+++ qt_test_driver/QtTestDriver/SignalingSocket.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+class SignalingSocket : public QObject {
+  Q_OBJECT
+
+ public:
+  explicit SignalingSocket(QObject* parent = nullptr);
+
+  // 启动监听作为 callee
+  void startListening(quint16 port);
+
+  // 作为 caller 连接到远端
+  void connectToHost(const QString& ip, quint16 port);
+
+  // 发送 JSON 字符串
+  void sendMessage(const QString& json);
+
+ signals:
+  void messageReceived(const QString& json);
+  void connected();
+  void disconnected();
+  void errorOccurred(const QString& error);
+
+ private slots:
+  void onNewConnection();
+  void onReadyRead();
+  void onSocketDisconnected();
+  void onSocketError(QAbstractSocket::SocketError socketError);
+
+ private:
+  QTcpServer* server_ = nullptr;
+  QTcpSocket* socket_ = nullptr;
+  QByteArray buffer_;
+};
